94f7cb47e731eb04923a419d1ac4326b722d613f
Matthew Feickert <matthew.feickert@cern.ch>
2021-06-17 22:57:22 -0500

    Use HOME for less typing

--- bluewaters/aprun_drell-yan.sh
+++ bluewaters/aprun_drell-yan.sh
@@ -4,11 +4,12 @@ if [ -d user_scratch ];then
   rm -rf user_scratch/*
 fi
 
+# $HOME is /u/sciteam/${USER}
 aprun -b -N 1 -cc none \
   -- shifter \
     --clearenv \
     --image=neubauergroup/bluewaters-mg5_amc:3.1.1 \
-    --volume=/mnt/a/u/sciteam/"${USER}"/user_scratch:/home/docker/data \
-    --volume=/mnt/a/u/sciteam/"${USER}":/mnt/a/u/sciteam/"${USER}" \
+    --volume=/mnt/a/"${HOME}"/user_scratch:/home/docker/data \
+    --volume=/mnt/a/"${HOME}":/mnt/a/"${HOME}" \
     --workdir=/home/docker/data \
-    -- /bin/bash -c 'mg5_aMC /mnt/a/u/sciteam/'"${USER}"'/MadGraph5-simulation-configs/configs/madgraph5/drell-yan.mg5'
+    -- /bin/bash -c 'mg5_aMC /mnt/a/'"${HOME}"'/MadGraph5-simulation-configs/configs/madgraph5/drell-yan.mg5'
