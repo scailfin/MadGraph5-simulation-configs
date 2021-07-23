92206bff85a00f95daadf3e369e347aacca7ce2b
Matthew Feickert <matthew.feickert@cern.ch>
2021-07-23 01:42:45 -0500

    Add start of stuff

---
+++ debug-example/drell-yan_example.cxx
@@ -0,0 +1,58 @@
+#include <momemta/ConfigurationReader.h>
+#include <momemta/Logging.h>
+#include <momemta/MoMEMta.h>
+#include <momemta/Unused.h>
+
+#include <TTree.h>
+#include <TChain.h>
+#include <TTreeReader.h>
+#include <TTreeReaderValue.h>
+#include <Math/PtEtaPhiM4D.h>
+#include <Math/LorentzVector.h>
+
+#include <chrono>
+#include <memory>
+
+#include <iostream>
+
+using namespace std::chrono;
+
+using LorentzVectorM = ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float>>;
+
+/*
+ * Example executable file loading an input sample of events,
+ * computing weights using MoMEMta in the Drell-Yan hypothesis,
+ * and saving these weights along with a copy of the event content in an output file.
+ */
+
+void normalizeInput(LorentzVector& p4) {
+    if (p4.M() > 0)
+        return;
+
+    // Increase the energy until M is positive
+    p4.SetE(p4.P());
+    while (p4.M2() < 0) {
+        double delta = p4.E() * 1e-5;
+        p4.SetE(p4.E() + delta);
+    };
+}
+
+int main(int argc, char** argv) {
+
+    UNUSED(argc);
+    UNUSED(argv);
+
+    using std::swap;
+
+    /*
+     * Load events from input file, retrieve reconstructed particles and MET
+     */
+    TChain chain("t");
+    // chain.Add("figure out how to make this a configurable path.root");
+    chain.Add("/data/hepmc_output/delphes_output/delphes_output_nevent_10e4.root");
+    TTreeReader myReader(&chain);
+
+    std::cout << "chain" << chain << "\n";
+
+    return 0;
+}
---
+++ debug-example/drell-yan_example.lua
@@ -0,0 +1,3 @@
+-- Load the library containing the matrix element
+-- FIXME: Requires manual configuration
+load_modules('MatrixElements/pp_drell_yan/build/libme_pp_drell_yan.so')
