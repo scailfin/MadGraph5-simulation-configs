cd326b1c0aedd13dd73b5aa9bae12d2701906cd7
Matthew Feickert <matthew.feickert@cern.ch>
2021-07-23 01:45:43 -0500

    Try to get build working

--- debug-example/run_momemta.sh
+++ debug-example/run_momemta.sh
@@ -24,19 +24,19 @@ cmake --build MatrixElements/pp_drell_yan/build \
     --clean-first \
     --parallel $(($(nproc) - 1))
 
-# # Example level build
+# Example level build
 # popd
-# if [ -d build ];then
-#   rm -rf build
-# fi
-# cmake \
-#     -DCMAKE_INSTALL_PREFIX=/usr/local/venv \
-#     -S . \
-#     -B build
-# cmake build -L
-# cmake --build build \
-#     --clean-first \
-#     --parallel $(($(nproc) - 1))
+if [ -d build ];then
+  rm -rf build
+fi
+cmake \
+    -DCMAKE_INSTALL_PREFIX=/usr/local/venv \
+    -S . \
+    -B build
+cmake build -L
+cmake --build build \
+    --clean-first \
+    --parallel $(($(nproc) - 1))
 
 # ls -lhtra build/momemta/
 # # Current configuration in drell_yan.cxx requires running from top level of example dir
