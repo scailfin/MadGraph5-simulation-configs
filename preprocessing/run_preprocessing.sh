91db241f11fcb1565bee910eb8679d25978d1f70
Matthew Feickert <matthew.feickert@cern.ch>
2021-07-29 03:34:12 -0500

    Use spaces not tabs

--- preprocessing/run_preprocessing.sh
+++ preprocessing/run_preprocessing.sh
@@ -4,9 +4,9 @@ export ROOT_INCLUDE_PATH="/usr/local/venv/include"
 # User input with default path
 INPUT_PATH="${1:-/data/hepmc_output/delphes_output/delphes_output_nevent_10e4.root}"
 python scripts/SimpleAna.py \
-	--input "${INPUT_PATH}" \
-	--output test.root \
-	--lumi 1000.0 \
-	--XS 0
+    --input "${INPUT_PATH}" \
+    --output test.root \
+    --lumi 1000.0 \
+    --XS 0
 
 unset ROOT_INCLUDE_PATH
