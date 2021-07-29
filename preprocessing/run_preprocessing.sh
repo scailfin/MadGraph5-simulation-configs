b267cfc62a13b177cf32e7385b4329c6012d9714
Matthew Feickert <matthew.feickert@cern.ch>
2021-07-29 01:14:05 -0500

    Allow for user input of INPUT_PATH

--- preprocessing/run_preprocessing.sh
+++ preprocessing/run_preprocessing.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-export INPUT_PATH="/data/hepmc_output/delphes_output/delphes_output_nevent_10e4.root"
+# User input with default path
+INPUT_PATH="${1:-/data/hepmc_output/delphes_output/delphes_output_nevent_10e4.root}"
 python scripts/SimpleAna.py \
 	--input "${INPUT_PATH}" \
 	--output test.root \
 	--lumi 1000.0 \
 	--XS 0
-
-unset INPUT_PATH
