cbfc6e8f37186b28fc160a0c7307c271bcc959c3
Matthew Feickert <matthew.feickert@cern.ch>
2021-08-01 23:43:55 -0500

    try with new file

--- debug-example/drell-yan_example.cxx
+++ debug-example/drell-yan_example.cxx
@@ -47,10 +47,10 @@ int main(int argc, char** argv) {
     /*
      * Load events from input file, retrieve reconstructed particles and MET
      */
-    TChain chain("Delphes");
+    TChain chain("event_selection/hftree");
     // chain.Add("figure out how to make this a configurable path.root");
     // Path needs to be findable inside of Docker container
-    chain.Add("/data/hepmc_output/delphes_output/delphes_output_nevent_10e4.root");
+    chain.Add("/home/feickert/workarea/MadGraph5-simulation-configs/preprocessing/preprocessing_output.root");
     TTreeReader myReader(&chain);
 
     std::cout << "chain " << &chain << "\n";
@@ -118,13 +118,15 @@ int main(int argc, char** argv) {
     //    ...
 
 
-    TTreeReaderValue<LorentzVectorM> lep_plus_p4M(myReader, "lep1_p4");
-    TTreeReaderValue<LorentzVectorM> lep_minus_p4M(myReader, "lep2_p4");
+    // TTreeReaderValue<LorentzVectorM> lep_plus_p4M(myReader, "lep1_p4");
+    // TTreeReaderValue<LorentzVectorM> lep_minus_p4M(myReader, "lep2_p4");
+
+    TTreeReaderValue<float> lep_plus_Pt(myReader, "lep1_Pt");
 
     /*
      * Define output TTree, which will contain the weights we're computing (including uncertainty and computation time)
      */
-    std::unique_ptr<TTree> out_tree = std::make_unique<TTree>("Delphes", "Delphes");
+    std::unique_ptr<TTree> out_tree = std::make_unique<TTree>("momemta", "momemta");
     double weight_DY, weight_DY_err, weight_DY_time;
     out_tree->Branch("weight_DY", &weight_DY);
     out_tree->Branch("weight_DY_err", &weight_DY_err);
@@ -151,8 +153,10 @@ int main(int argc, char** argv) {
          * We define here Particles, allowing MoMEMta to correctly map the inputs to the configuration file.
          * The string identifier used here must be the same as used to declare the inputs in the config file
          */
-        momemta::Particle lep_plus("lepton1",  LorentzVector { lep_plus_p4M->Px(), lep_plus_p4M->Py(), lep_plus_p4M->Pz(), lep_plus_p4M->E() });
-        momemta::Particle lep_minus("lepton2", LorentzVector { lep_minus_p4M->Px(), lep_minus_p4M->Py(), lep_minus_p4M->Pz(), lep_minus_p4M->E() });
+        // momemta::Particle lep_plus("lepton1",  LorentzVector { lep_plus_p4M->Px(), lep_plus_p4M->Py(), lep_plus_p4M->Pz(), lep_plus_p4M->E() });
+        // momemta::Particle lep_minus("lepton2", LorentzVector { lep_minus_p4M->Px(), lep_minus_p4M->Py(), lep_minus_p4M->Pz(), lep_minus_p4M->E() });
+
+        std::cout << "lep_plus_Pt " << lep_plus_Pt << "\n";
     }
 
     // ...
