0dc30436c906264011bf315f22dde4732e94a10b
Matthew Feickert <matthew.feickert@cern.ch>
2021-08-02 01:49:11 -0500

    add note on where copied most of example from

--- debug-example/drell-yan_example.lua
+++ debug-example/drell-yan_example.lua
@@ -1,3 +1,5 @@
+-- c.f. https://github.com/MoMEMta/Tutorials/blob/master/Paper_configs/MELA_ZZ_bkg.lua for example copied form from
+
 -- Load the library containing the matrix element
 -- FIXME: Requires manual configuration
 load_modules('MatrixElements/pp_drell_yan/build/libme_pp_drell_yan.so')
