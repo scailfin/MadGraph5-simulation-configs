4728a9483e154d4b771b289489e7c70eae55ebfa
Matthew Feickert <matthew.feickert@cern.ch>
2021-08-01 22:47:44 -0500

    add output path variable

--- preprocessing/run_preprocessing.sh
+++ preprocessing/run_preprocessing.sh
@@ -5,9 +5,12 @@ export ROOT_INCLUDE_PATH="/usr/local/venv/include"
 
 # User input with default path
 INPUT_PATH="${1:-delphes_output.root}"
+OUTPUT_PATH=${2:-preprocessing_output.root}
+# Lumi is currently set to a default value and should be updated to something
+# more relevant
 python scripts/SimpleAna.py \
     --input "${INPUT_PATH}" \
-    --output test.root \
+    --output "${OUTPUT_PATH}" \
     --lumi 1000.0 \
     --XS 0
 
