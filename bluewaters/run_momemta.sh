cf0c77714c27520862ab2dbfbafe521e13f64ca9
Matthew Feickert <matthew.feickert@cern.ch>
2021-08-13 12:41:20 -0500

    parallelize MoMEMta across 200 jobs with qsub -v

--- bluewaters/run_momemta.sh
+++ bluewaters/run_momemta.sh
@@ -7,4 +7,18 @@ if [ "${BASH_VERSION:0:1}" -lt 4 ]; then
 fi
 
 PROCESS_DIRECTORY="${1:-drell-yan}"
-qsub "${PROCESS_DIRECTORY}/momemta.pbs"
+
+NUMBER_OF_JOBS=200
+# Submit jobs
+echo "# Submitting ${NUMBER_OF_JOBS} jobs"
+echo ""
+for n_job in $(seq 0 $((${NUMBER_OF_JOBS}-1)))
+do
+    # qsub -v: comma separated list of strings of the form variable or variable=value.
+    qsub -v NUMBER_OF_STEPS="${NUMBER_OF_JOBS}",STEP_NUMBER="${n_job}" "${PROCESS_DIRECTORY}/momemta.pbs"
+done
+echo ""
+echo "# Submitted ${NUMBER_OF_JOBS} jobs"
+echo ""
+echo '# Check status with: qstat -u $USER'
+qstat -u "${USER}"
