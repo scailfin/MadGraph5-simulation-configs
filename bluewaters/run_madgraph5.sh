639c924985bd50004325321fba18e2e10925fc2d
Matthew Feickert <matthew.feickert@cern.ch>
2021-08-13 11:17:52 -0500

    chore: Ensure submission scripts have modern Bash (#14)
    * Add check on Bash version in submission scripts and warn user and exit 1 if the version of Bash is not at least v4.0
       - Bash v4.x+ has different syntax for what is a valid for loop compared to v3.0

--- bluewaters/run_madgraph5.sh
+++ bluewaters/run_madgraph5.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "${BASH_VERSION:0:1}" -lt 4 ]; then
+    echo "ERROR: This script uses Bash version 4.0+ syntax. Your Bash version is ${BASH_VERSION} which is too old."
+    echo "       Run: 'module load bash' to get a modern version on Blue Waters."
+    exit 1
+fi
+
 PROCESS_DIRECTORY="${1:-drell-yan}"
 RANDOM_SEED=300
 qsub "${PROCESS_DIRECTORY}/madgraph5.pbs"
