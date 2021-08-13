7372dfa702413c1cd3001c05cd66868cc2ea981d
Matthew Feickert <matthew.feickert@cern.ch>
2021-08-13 11:12:03 -0500

    Update other run files

--- bluewaters/run_delphes.sh
+++ bluewaters/run_delphes.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ "${BASH_VERSION:0:1}" -lt 4 ]; then
+    echo "ERROR: This script uses Bash version 4.0+ syntax. Your Bash version is ${BASH_VERSION} which is too old."
+    echo "       Run: 'module load bash' to get a modern version on Blue Waters."
+    exit 1
+fi
+
 PROCESS_DIRECTORY="${1:-drell-yan}"
 qsub "${PROCESS_DIRECTORY}/delphes.pbs"
