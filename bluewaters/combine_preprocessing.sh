a07bfdd1703f76a682b31225be332457654d6280
Matthew Feickert <matthew.feickert@cern.ch>
2021-08-18 10:37:01 -0500

    feat: Add preprocessing combination script (#21)
    * Add combination script for job that uses ROOT's `hadd` to combine all the ROOT output files from the preprocessing stage into a single ROOT file
       - Use hadd -j option to parallelize the execution in multiple processes
    * Add user level submission script

---
+++ bluewaters/combine_preprocessing.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "${BASH_VERSION:0:1}" -lt 4 ]; then
+    echo "ERROR: This script uses Bash version 4.0+ syntax. Your Bash version is ${BASH_VERSION} which is too old."
+    echo "       Run: 'module load bash' to get a modern version on Blue Waters."
+    exit 1
+fi
+
+PROCESS_DIRECTORY="${1:-drell-yan_ll}"
+
+qsub "${PROCESS_DIRECTORY}/combine_preprocessing.pbs"
+echo ""
+echo '# Check status with: qstat -u $USER'
+qstat -u "${USER}"
