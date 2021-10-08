15740c3f08077c79cac5413ce115932b351e5b6b
Matthew Feickert <matthew.feickert@cern.ch>
2021-10-08 15:48:34 -0500

    Update run

--- dgx/run_madgraph.sh
+++ dgx/run_madgraph.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-IMAGE="scailfin/madgraph5-amc-nlo-centos:mg5_amc3.2.0"
+# IMAGE="scailfin/madgraph5-amc-nlo-centos:mg5_amc3.2.0"
+IMAGE="neubauergroup/bluewaters-mg5_amc:3.1.1"
 docker pull "${IMAGE}"
 
-docker run --rm -ti "${IMAGE}"
+docker run \
+    --rm \
+    -ti \
+    -v "$PWD/..":/root/inputs \
+    -v "$PWD":/root/data/ \
+    --privileged=true \
+    "${IMAGE}"
