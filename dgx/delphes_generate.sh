826befa7ba56f41cb37be6167429a408b9917753
Matthew Feickert <matthew.feickert@cern.ch>
2021-10-08 15:48:01 -0500

    Update delphes

--- dgx/delphes_generate.sh
+++ dgx/delphes_generate.sh
@@ -9,20 +9,13 @@ if [[ ! -d "outputs/delphes" ]]; then
 fi
 cd outputs/delphes
 
-# INPUT_FILE_PATH="${BASE_PREFIX}/outputs/madgraph/${PHYSICS_PROCESS}_output/Events/run_01/tag_1_pythia8_events.hepmc.gz"
 INPUT_FILE_PATH="${BASE_PREFIX}/outputs/madgraph/${PHYSICS_PROCESS}_output/Events/run_01/tag_1_pythia8_events.hepmc"
 
 printf "\n# printenv:\n"
 printenv
 printf "\n\n"
 
-# ls -lhtra "${INPUT_FILE_PATH::-3}.gz"
-
 printf "# Unzip HEPMC file if needed\n\n" && \
-# if [ -f "${INPUT_FILE_PATH::-3}.gz" ]; then
-#     export INPUT_FILE_PATH="${INPUT_FILE_PATH::-3}"
-#     gunzip "${INPUT_FILE_PATH}.gz"
-# fi
 if [ -f "${INPUT_FILE_PATH}.gz" ]; then
     gunzip "${INPUT_FILE_PATH}.gz"
 fi
--- dgx/run_delphes.sh
+++ dgx/run_delphes.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# IMAGE="scailfin/madgraph5-amc-nlo-centos:mg5_amc3.2.0"
 IMAGE="scailfin/delphes-python-centos:3.5.0"
 docker pull "${IMAGE}"
 
