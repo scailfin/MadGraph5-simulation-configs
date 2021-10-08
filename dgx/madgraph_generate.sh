4004215a584bb5827905e50318557de3917a2ade
Matthew Feickert <matthew.feickert@cern.ch>
2021-10-08 12:03:36 -0500

    Change outputs name

--- dgx/madgraph_generate.sh
+++ dgx/madgraph_generate.sh
@@ -4,16 +4,18 @@ INPUTS_DIR=/root/inputs
 PHYSICS_PROCESS="drell-yan_ll"
 CONFIG_NAME="${PHYSICS_PROCESS}.json"
 
-if [[ ! -d "outputs" ]]; then
-    mkdir -p outputs
+if [[ ! -d "outputs/madgraph" ]]; then
+    mkdir -p outputs/madgraph
 fi
-cd outputs
+cd outputs/madgraph
 
 printf "\n# printenv:\n"
 printenv
 printf "\n\n"
 
 python "${INPUTS_DIR}/generate_config.py" --help
+
+echo ""
 
 python \
     "${INPUTS_DIR}"/generate_config.py \
@@ -22,14 +24,7 @@ python \
         --seed 0
 
 cat "${PHYSICS_PROCESS}.mg5"
+
+echo ""
+
 time mg5_aMC "${PHYSICS_PROCESS}".mg5
-
-# -- /bin/bash -c 'export PATH="/usr/local/venv/bin:${PATH}" && \
-#     printf "\n# printenv:\n" && printenv && printf "\n\n" && \
-#     python "${CODE_BASE_PATH}"/generate_config.py --help && \
-#     echo "" && \
-#     python "${CODE_BASE_PATH}"/generate_config.py -c "${CODE_BASE_PATH}/configs/json/${CONFIG_NAME}" --outpath $PWD --seed "${RANDOM_SEED}" && \
-#     cat "${PHYSICS_PROCESS}".mg5 && \
-#     echo "" && \
-#     time mg5_aMC "${PHYSICS_PROCESS}".mg5'
-
